679e28c7bb210d222790894281f0e24ce6cb0bb7
Douglas Renwick <37882040+dorenwick@users.noreply.github.com>
2024-03-04 22:43:06 +1300

    Add files via upload

--- Dataset_Merge.py
+++ Dataset_Merge.py
@@ -1,59 +1,75 @@
+from datasets import concatenate_datasets, ClassLabel, DatasetDict, load_from_disk, Sequence
 
-from datasets import concatenate_datasets, ClassLabel, DatasetDict
 
-
+def update_ner_tags(example, class_label):
+    updated_ner_tags = []
+    for label in example['ner_tags']:
+        if isinstance(label, int):
+            # If the label is already an integer, use it directly
+            updated_ner_tags.append(label)
+        else:
+            # If the label is a string, convert it to an integer
+            updated_ner_tags.append(class_label.str2int(label))
+    return {'ner_tags': updated_ner_tags}
 
 class DatasetMerge:
     def __init__(self, datasets):
         self.datasets = datasets
 
-    """
-    For this class, it will essentially do the opposite of what DatasetSplit did.
-
-    This class will take a list of datasets,
-    and each dataset past the first one must have the exact same Features structure (same datatypes, i.e, Sequence, Value, dtype, id, ect).
-    The specified class-labels, however, are allowed to differ.
-
-    So, the above description will be our first method.
-
-    Now, given that all the datasets are the same features type. We will want to pool all the ClassLabels together.
-    We will also have to relabel and realign the id2labels, if that makes sense.
-
-    So for example if one dataset has ClassLabels: O, ORG, PER and the other has classLabels O, MISC, LOC
-    then id2label in class one is 0: O, 1: ORG, 2:PER
-    and id2label in class two is 0: O, 1: MISC, 2: LOC
-
-    we will want to end up, after merging, with something like this:
-
-    0: O, 1: ORG, 2:PER 3: MISC, 4: LOC.
-
-    Understand?
-
-    Tell me how to accomplish this.
-
-
-    """
-
     def merge(self):
-        # Verify that all datasets have the same feature structure as the first dataset
-        first_dataset_features = self.datasets[0].features
+        # Verify that all datasets have the same feature structure as the first dataset (ignoring class labels)
+        first_dataset_features = self._get_features(self.datasets[0], ignore_labels=True)
         for dataset in self.datasets[1:]:
-            if dataset.features != first_dataset_features:
-                raise ValueError("All datasets must have the same feature structure as the first dataset")
+            if self._get_features(dataset, ignore_labels=True) != first_dataset_features:
+                raise ValueError("All datasets must have the same feature structure as the first dataset (ignoring class labels)")
+
+        # Prepare datasets for concatenation by updating class labels
+        updated_datasets = []
+        all_labels = set()
+        for dataset in self.datasets:
+            all_labels.update(self._get_features(dataset)['ner_tags'].feature.names)
+
+        new_class_label = ClassLabel(names=sorted(all_labels), num_classes=len(all_labels))
+        for dataset in self.datasets:
+            updated_dataset = dataset.map(lambda example: update_ner_tags(example, new_class_label))
+            # Update the ner_tags feature in the dataset's features
+            updated_features = updated_dataset.features
+            updated_features['ner_tags'] = Sequence(feature=new_class_label)
+            updated_dataset = updated_dataset.cast(updated_features)
+            updated_datasets.append(updated_dataset)
 
         # Concatenate the datasets
-        merged_dataset = concatenate_datasets(self.datasets)
-
-        # Merge ClassLabels and reassign labels
-        if 'label' in merged_dataset.features and isinstance(merged_dataset.features['label'], ClassLabel):
-            all_labels = set()
-            for dataset in self.datasets:
-                all_labels.update(dataset.features['label'].names)
-
-            # Create a new ClassLabel feature with the merged labels
-            new_class_label = ClassLabel(names=sorted(all_labels))
-
-            # Update the labels in the merged dataset
-            merged_dataset = merged_dataset.map(lambda example: {'label': new_class_label.str2int(example['label'])})
+        merged_dataset = concatenate_datasets(updated_datasets)
 
         return merged_dataset
+
+    def _get_features(self, dataset, ignore_labels=False):
+        # If the dataset is a DatasetDict, use the first split's features
+        if isinstance(dataset, DatasetDict):
+            features = next(iter(dataset.values())).features
+        else:
+            features = dataset.features
+
+        # Ignore class labels if requested
+        if ignore_labels and 'ner_tags' in features and isinstance(features['ner_tags'].feature, ClassLabel):
+            features = features.copy()
+            del features['ner_tags']
+
+        return features
+
+# Load the datasets
+dataset_one = load_from_disk("C:/Users/doren/AppData/Roaming/Gantrithor/data/datasets/saved_dataset_one")['train']
+dataset_two = load_from_disk("C:/Users/doren/AppData/Roaming/Gantrithor/data/datasets/saved_dataset_two")['train']
+
+# Create a list of the datasets
+datasets = [dataset_one, dataset_two]
+
+# Create an instance of the DatasetMerge class
+merger = DatasetMerge(datasets)
+
+# Merge the datasets
+merged_dataset = merger.merge()
+
+# Print some information about the merged dataset
+print("Number of examples in the merged dataset:", len(merged_dataset))
+print("Features of the merged dataset:", merged_dataset.features)
